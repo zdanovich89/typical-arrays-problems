ba08c7414ef8df64b56303dadbed693ef86364f2
zdanovich89 <zdanovich89@gmail.com>
2020-02-27 00:15:32 +0300

    feat: create functions for finding average, maximum and minimum numbers

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array !== undefined || Array.isArray(array)) {
+        if(array.length > 0) {
+            return array.sort((a, b) => a - b)[0];
+        }
+        else return 0;
+    }
+    else return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array !== undefined || Array.isArray(array)) {
+        if(array.length > 0) {
+            return array.sort((a, b) => b - a)[0];
+        }
+        else return 0;
+    }
+
+    else return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(array !== undefined || Array.isArray(array)) {
+        if(array.length > 0) {
+            let sum = array.reduce((sum, item) => sum + item, 0);
+            return sum / array.length;
+        }
+        else return 0;
+    }
+    else return 0;
 }
